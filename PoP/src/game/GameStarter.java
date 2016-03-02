144394a860eab5e0db59775a2cd9bc8e87a5c8f2
Alejandro <donrondon40@gmail.com>
2016-03-02 15:11:20 +0100

    Loader included at game start

--- PoP/src/game/GameStarter.java
+++ PoP/src/game/GameStarter.java
@@ -2,11 +2,17 @@ package game;
 
 import javax.swing.JFrame;
 
+import framework.Loader;
+
 public class GameStarter {
 	
 	public static void main(String[] args){
 		SplashScreen s = new SplashScreen("resources/Sprites_400/Splash/splash.png");
 		s.showSplashScreen(true);
+		
+		/* Loads every sprite in the game */
+		Loader loader = new Loader(1000/14);
+		loader.loadAllSprites();
 		
 		JFrame window = new JFrame("Prince Of Persia");
 		window.setContentPane(new Game());

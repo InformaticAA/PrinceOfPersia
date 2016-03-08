5213bd9511ce42ca84e555e28f089383a58f6ecc
Alejandro Márquez <alejandromarquez94@gmail.com>
2016-03-08 15:47:23 +0100

    Starting to implement loose floors

--- PoP/src/entities/LooseFloor.java
+++ PoP/src/entities/LooseFloor.java
@@ -4,8 +4,8 @@ import framework.Loader;
 
 public class LooseFloor extends Entity {
 
-	public LooseFloor(int x, int y, Loader loader) {
-		super(x, y, loader);
+	public LooseFloor(int x, int y, int x_offset, int y_offset, Loader loader, boolean activated, String loose_type) {
+		super(x+x_offset, y+y_offset, loader);
 	}
 	
 }

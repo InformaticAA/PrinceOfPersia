f4bdbf9d4e4af5803e60515de28709ab674f6e93
AMarquez94 <alejandromarquez94@gmail.com>
2016-08-04 23:53:53 +0200

    Door animations added (To do: fix close animation and buttons)

--- PoP/src/entities/Door.java
+++ PoP/src/entities/Door.java
@@ -8,15 +8,20 @@ public class Door extends Entity{
 	private int id;
 	private Sound opening;
 	private Sound closing;
+	private Sound closing_fast;
+	private Sound closed;
+	private int how_opened;
 	
 	public Door(int x, int y, int x_offset, int y_offset, Loader loader, String door_state, int id, int frame) {
 		super("Door", x + x_offset, y + y_offset, loader);
 		this.id = id;
 		animations = loader.getAnimations("door");
-		currentAnimation = animations.get(door_state);
+		this.setCurrentAnimation(door_state, FRAME_DURATION*3);
 		currentAnimation.setCurrentFrame(frame);
-		this.opening = loader.getSound("gate opening");
-		this.closing = loader.getSound("gate closing");
+		opening = loader.getSound("gate opening");
+		closing = loader.getSound("gate closing");
+		closing_fast = loader.getSound("gate closing_fast");
+		closed = loader.getSound("gate stop");
 	}
 	
 	public int getId() {
@@ -30,12 +35,49 @@ public class Door extends Entity{
 	@Override
 	public void update(long elapsedTime){
 		super.update(elapsedTime);
-		if(currentAnimation.getId().equals("door_opening")){
-			opening.play();
-		} else if(currentAnimation.getId().equals("door_closing")){
-			closing.play();
-		}
+		switch(currentAnimation.getId()){
 		
+		case "door_closed":
+			
+			break;
+		
+		case "door_half_opening":
+			if(this.currentAnimation.isLastFrame()){
+				opening.play();
+			}
+			if(this.getCurrentAnimation().isOver(false)){
+				this.setCurrentAnimation("door_opening", FRAME_DURATION*3);
+			}
+			break;
+			
+		case "door_opening":
+			if(this.currentAnimation.isLastFrame()){
+				opening.play();
+			}
+			if(this.getCurrentAnimation().isOver(false)){
+				this.setCurrentAnimation("door_opened", FRAME_DURATION*3);
+			}
+			break;
+		
+		case "door_closing":
+			if(this.currentAnimation.isLastFrame()){
+				closing.play();
+			}
+			if(this.getCurrentAnimation().isOver(false)){
+				this.setCurrentAnimation("door_half_closing", FRAME_DURATION*3);
+			}
+			break;
+			
+		case "door_half_closing":
+			if(this.currentAnimation.isLastFrame()){
+				closing.play();
+			}
+			if(this.getCurrentAnimation().isOver(false)){
+				closed.play();
+				this.setCurrentAnimation("door_closed", FRAME_DURATION*3);
+			}
+			break;
+		}
 	}
 
 	@Override

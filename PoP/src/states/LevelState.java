e11d9eb865c9a5af2b590014811194424c10be94
Alejandro <donrondon40@gmail.com>
2016-08-21 13:49:21 +0200

    Bugs in 2-4 and 2-5 fixed. TODO: Loose dont have to create corner if there is a wall-type object in that square.

--- PoP/src/states/LevelState.java
+++ PoP/src/states/LevelState.java
@@ -82,12 +82,12 @@ public class LevelState extends State{
 			/* Start game */
 			remainingTime = INIT_TIME;
 			currentLevel = loader.loadLevel(INITIAL_LEVEL);
-			currentRoom = currentLevel.getRoom(2,5);
+			currentRoom = currentLevel.getRoom(2,4);
 			doors = currentLevel.getDoors();
 
-			player = new Player(600,110,loader, 3, "left"); // primer piso
+//			player = new Player(600,110,loader, 3, "left"); // primer piso
 //			player = new Player(600,240,loader, 3, "left"); // segundo piso
-//			player = new Player(400,370,loader, 3, "left"); // tercer piso
+			player = new Player(400,370,loader, 3, "left"); // tercer piso
 			player.setCurrentAnimation("idle_left", 5);
 //			player = new Player(500,100,loader, 3, "left");
 //			player.setCurrentAnimation("falling_left", 5);
